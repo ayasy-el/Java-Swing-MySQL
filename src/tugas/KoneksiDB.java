babb9566e482476178189bc11c3b125042065b72
ayasy <ayasy.elg@gmail.com>
2024-11-12 23:35:47 +0700

    + tugas

---
+++ src/tugas/KoneksiDB.java
@@ -0,0 +1,39 @@
+package tugas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class KoneksiDB {
+//    public static void main(String[] args) {
+//        // Define the connection
+//        Connection koneksi = null;
+//        try {
+//            koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "");
+//            System.out.println("Koneksi Berhasil");
+//        } catch (SQLException ex) {
+//            System.err.println("Error: " + ex);
+//            System.exit(1);
+//        }
+//    }
+    
+    public static Connection getConnection() {
+        Connection koneksi = null;
+        try {
+            koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/itemlist", "root", "");
+            System.out.println("Koneksi Berhasil");
+        } catch (SQLException ex) {
+            System.err.println("Error: " + ex.getMessage());
+        }
+        return koneksi;
+    }
+    
+    public static void main(String[] args) {
+        Connection koneksi = KoneksiDB.getConnection();
+        if (koneksi != null) {
+            System.out.println("Koneksi berhasil di dalam itemlist");
+        } else {
+            System.out.println("Koneksi gagal di dalam itemlist");
+        }
+    }
+}
